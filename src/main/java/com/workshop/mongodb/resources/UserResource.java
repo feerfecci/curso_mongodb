04d896d8425cb0fc32e2f59e316c95da2d103230
Fernando Fecci <fernandofecci@hotmail.com>
2026-08-18 11:11:09 -0300

    delete user

--- src/main/java/com/workshop/mongodb/resources/UserResource.java
+++ src/main/java/com/workshop/mongodb/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,6 +53,12 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
         return ResponseEntity.created(uri).body(new UserDTO(obj));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/workshop/mongodb/services/UserService.java
+++ src/main/java/com/workshop/mongodb/services/UserService.java
@@ -30,4 +30,9 @@ public class UserService {
     public User insert(User obj) {
         return userRepository.insert(obj);
     }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
+    }
 }
